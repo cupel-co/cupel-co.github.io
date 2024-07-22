c8b028f658dc8c4c8b7fd725f375ed71729ff4c0
Gavan Lamb <gavan.jon.lamb@gmail.com>
2024-07-22 23:07:01 +1000

    feat(GH-1): configure services page

--- src/src/app/services/page.tsx
+++ src/src/app/services/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Navbar from "../../components/Layout/Navbar";
 import PageHeader from "../../components/Common/PageHeader";
-import WhyChooseUs from "../../components/Services/WhyChooseUs";
-import WhatWeDo from "../../components/Services/WhatWeDo";
+import ServicesOverview from "../../components/ServicesTwo/ServicesOverview";
+import OurServices from "../../components/ServicesTwo/OurServices";
 import Footer from "../../components/Layout/Footer";
 
 export default function Page() {
@@ -11,15 +11,15 @@ export default function Page() {
       <Navbar />
 
       <PageHeader
-        pageTitle="Services Style One"
+        pageTitle="IT Services"
         breadcrumbTextOne="Home"
         breadcrumbUrl="/"
-        breadcrumbTextTwo="Services Style One"
+        breadcrumbTextTwo="IT Services"
       />
 
-      <WhyChooseUs />
+      <ServicesOverview />
 
-      <WhatWeDo />
+      <OurServices />
 
       <Footer />
     </>
